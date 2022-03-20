d2f6d69760b14db5af1003b4c3fed2a9b1b632b3
Choiyu330 <yuja40@ks.ac.kr>
2022-03-20 23:34:16 +0900

    기본변수 공부

---
+++ app/src/main/java/com/example/kotlin/Kotlin/03.Practice.kt
@@ -0,0 +1,22 @@
+package com.example.kotlin.Kotlin
+
+var a = 1 + 2 + 3 + 4 + 5 //연산의 결과값을 변수에 넣어 줄 수 있다
+var b = "1"
+var c = b.toInt()
+var d = b.toFloat()
+
+var e = "YuJin"
+var f = "My name is $e Nice to meet you"
+
+fun main(array: Array<String>) {
+    println(a)
+    println(b)
+    println(c)
+    println(d)
+    println(e)
+    println(f)
+
+
+
+
+}
