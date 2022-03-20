66283d0a5a26b44e6a84092aeab6b9f51e5678c7
Choiyu330 <yuja40@ks.ac.kr>
2022-03-20 23:59:52 +0900

    기본변수 공부 + null 추가

--- app/src/main/java/com/example/kotlin/Kotlin/03.Practice.kt
+++ app/src/main/java/com/example/kotlin/Kotlin/03.Practice.kt
@@ -8,6 +8,15 @@ var d = b.toFloat()
 var e = "YuJin"
 var f = "My name is $e Nice to meet you!"
 
+// Null
+// - 존재하지 않는다
+
+// var abc : Int = null
+// 아래와 같이 ? 를 붙이면 null을 자료형에 넣을 수 있음
+var abc1 : Int? = null
+var abc2 : Double? = null
+
+var g = a + 3
 fun main(array: Array<String>) {
     println(a)
     println(b)
@@ -15,5 +24,8 @@ fun main(array: Array<String>) {
     println(d)
     println(e)
     println(f)
+    println(abc1)
+
+    println(g)
 
 }
