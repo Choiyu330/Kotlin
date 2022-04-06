73285e6df0c6a9eb3e2041a7943efe531267b701
Choiyu330 <yuja40@ks.ac.kr>
2022-04-06 22:46:52 +0900

    220406 중간과제(2) 풀이

--- app/src/main/java/com/example/kotlin/Kotlin/19-1.Test.kt
+++ app/src/main/java/com/example/kotlin/Kotlin/19-1.Test.kt
@@ -1,6 +1,5 @@
 package com.example.kotlin.Kotlin
 
-// 1) 사칙 연산을 수행할 수 있는 클래스 ( +, -, *, / )
 
 // 2) 이행 계좌 만들기
 //      - 계좌생성 기능 (이름, 생년월일)
@@ -12,3 +11,37 @@ package com.example.kotlin.Kotlin
 //      - on/off 기능
 //      - 채널을 돌리는 기능
 //      - 초기채널은 (S사, M사, K사 총 3개)
+
+fun main(array: Array<String>) {
+
+    val cal: Cal = Cal(10, 2)
+    cal.plus()
+    cal.min()
+    cal.mul()
+    cal.div()
+}
+
+// 1) 사칙 연산을 수행할 수 있는 클래스 ( +, -, *, / )
+class Cal(var num1: Int, var num2: Int) {
+
+    fun plus() {
+        var result: Int = num1 + num2
+        println("$num1 + $num2 = $result")
+    }
+
+    fun min() {
+        var result: Int = num1 - num2
+        println("$num1 - $num2 = $result")
+    }
+
+    fun mul() {
+        var result: Int = num1 * num2
+        println("$num1 * $num2 = $result")
+    }
+
+    fun div() {
+        var result: Int = num1 / num2
+        println("$num1 / $num2 = $result")
+    }
+}
+
